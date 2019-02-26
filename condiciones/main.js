961b962a77a8a8bef6d64e10b8e8f7fcf0331b4e
angeloLuna <angel.luna.najera@gmail.com>
2019-02-25 21:50:03 -0600

    condiciones

---
+++ condiciones/main.js
@@ -0,0 +1,43 @@
+// let numero = 1000
+// let numero2 = 100
+
+// if(numero > numero2){
+//   console.log('verdadero')
+
+// }else{
+//   console.log('falso')
+// }
+
+
+// operadores logicos
+
+// < > <= >= !==
+
+
+
+// let nombre = prompt('Ingresa tu nombre')
+
+
+// if(nombre.length <= 3){
+//   console.log('super cortito')
+
+// }else if(nombre.length < 6){
+//   console.log('normal')
+
+// }else{
+//   console.log('super largo')
+// }
+
+// console.log(nombre.length)
+
+let numero = 9
+
+if(numero % 2 === 0) {
+  console.log('es par')
+}else {
+  console.log('es impar')
+}
+
+
+
+
