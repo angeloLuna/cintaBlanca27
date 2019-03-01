e0d4394da42affecfab6e755cd44d16ba80f70b7
angeloLuna <angel.luna.najera@gmail.com>
2019-02-28 21:54:01 -0600

    ciclos

---
+++ 10_ciclos/main.js
@@ -0,0 +1,31 @@
+
+// for(let i = 1 ; i <= 100; i++) {
+
+
+//   if(i % 2 === 0) {
+//     // imprimir si es par o impar
+//     // ECMA 6 interpolacion
+//     // console.log(`${i} es par`)
+//     console.log(i + ' es par')
+//   } else {
+//     console.log(i + ' es impar')
+//   }
+// }
+
+// 0 => -300 de uno en uno
+
+// 500 => 2000 de 5 en 5
+
+// 3000 => -3000 de 10 en 10
+
+
+// ------------------
+
+let letras = ['a', 't', 'e', 'd', 'x', 'z', 'e', 'd', 'x']
+
+for(let i = 0; i < letras.length; i++) {
+
+  console.log( letras[i] )
+
+}
+
